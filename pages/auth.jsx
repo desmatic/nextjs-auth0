b39ee7d6dacd9a3fb8de52306276584dc6d46301
desjones <desmond.jones@wearepatchworks.com>
2022-05-02 10:25:21 +0100

    test jose

--- pages/auth.jsx
+++ pages/auth.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { useRouter } from 'next/router'
+import * as jose from 'jose'
 
 export default function Index() {
   const router = useRouter()
   const qstring = router.query
+  const secretKey = '901b1717691b7e0521fef72fe067cbc119ff4eb980574654176e41a87adc0b28'
+  const { payload, protectedHeader } = await jose.jwtDecrypt(jwt, secretKey)
+  
   return (
     <>
       <pre>{JSON.stringify(qstring)}</pre>
+      <pre>{JSON.stringify(payload)}</pre>
       <a href={`https://dev-7mz8uunx.us.auth0.com/continue?state=${qstring.state}`} >continue</a>
     </>
   );
