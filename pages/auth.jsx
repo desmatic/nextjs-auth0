af0755a8b1273fbfbc25c86bfe25f77b7da9716e
desjones <desmond.jones@wearepatchworks.com>
2022-05-02 11:08:33 +0100

    test jose

--- pages/auth.jsx
+++ pages/auth.jsx
@@ -6,13 +6,19 @@ export default function Index() {
   const router = useRouter()
   const qstring = router.query
   const secretKey = '901b1717691b7e0521fef72fe067cbc119ff4eb980574654176e41a87adc0b28'
-  const { payload, protectedHeader } = jose.jwtDecrypt(qstring.session_token, secretKey)
+  
+  const getClaims = async (qstring, secretKey) => {
+    return await jose.jwtDecrypt(qstring.session_token, secretKey)
+  }
+
+  const jwt = getClaims(qstring, secretKey).then((data) => { return data }) 
   
   return (
     <>
       <pre>{JSON.stringify(qstring)}</pre>
       <pre>{JSON.stringify(qstring.session_token)}</pre>
       <pre>{JSON.stringify(secretKey)}</pre>
+      <pre>{JSON.stringify(jwt)}</pre>
       <a href={`https://dev-7mz8uunx.us.auth0.com/continue?state=${qstring.state}`} >continue</a>
     </>
   );
